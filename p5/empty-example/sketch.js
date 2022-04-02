3fb621a48b4887236786c8d6f72e39d4671958eb
IneffableKoD <34814092+IneffableKoD@users.noreply.github.com>
2022-04-02 17:49:42 +0200

    add and modify p5

---
+++ p5/empty-example/sketch.js
@@ -0,0 +1,12 @@
+// put setup code here
+
+function setup() {
+    createCanvas(400, 400);
+  }
+
+// put drawing code here  
+
+function draw() {
+    background(220);
+    ellipse(50,50,80,80);
+  }
